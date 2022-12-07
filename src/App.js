80a1ba93350b11da1252c907487f093959d4b296
ckwlghks123 <ckwlghks12+github2@gmail.com>
2022-12-08 05:10:05 +0900

    로그인 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import TodoList from "./pages/TodoList/TodoList";
+import SignIn from "./pages/SignIn/SignIn";
+import SignUp from "./pages/SignUp/SignUp";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/todo" element={<TodoList />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/SignUp/SignUp.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SignUp = () => {
+  return <h1>로그인 페이지</h1>;
+};
+
+export default SignUp;
